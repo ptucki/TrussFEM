8134479db304198f59f95255d729990960f589e0
ptucki <patryk.tucki@protonmail.com>
2023-12-19 20:58:00 +0100

    AddElement method fills node list

--- src/project.cpp
+++ src/project.cpp
@@ -1,23 +1,68 @@
 #include "project.h"
 
+#include <memory>
 #include <iostream>
 
 void Project::AddElement(Node<3> i, Node<3> j)
 {
   //Check if Node exists at such a location
+  std::shared_ptr<Node<3>> i_node = std::make_shared<Node<3>>(std::move(i));
+  std::shared_ptr<Node<3>> j_node = std::make_shared<Node<3>>(std::move(j));
+  auto end = nodes_.end();
 
-  //Add element if exists
-  nodes_.push_back(std::make_shared<Node<3>>(i));
-  nodes_.push_back(std::make_shared<Node<3>>(j));
-  auto before_last = nodes_.end() - 2;
-  auto last = nodes_.end() - 1;
-  elements_.emplace_back(std::make_shared<Element<3>>(*before_last, *last));
+  bool i_node_found = false;
+  bool j_node_found = false;
+
+  for (auto it = std::begin(nodes_); it != std::end(nodes_); ++it)
+  {
+    if (**it == i)
+    {
+      i_node_found = true;
+      i_node = *it;
+    }
+    if (**it == j)
+    {
+      j_node_found = true;
+      i_node = *it;
+    }
+  }
+
+  if (!i_node_found) nodes_.push_back(std::move(i_node));
+  if (!j_node_found) nodes_.push_back(std::move(j_node));
+
+  elements_.emplace_back(std::make_shared<Element<3>>(nodes_[nodes_.size() - 2], nodes_[nodes_.size() - 1]));
 }
 
-Project::ElementList& Project::GetElementList()
+Project::ElementList& Project::GetElementList() &
 {
   return elements_;
 }
+
+Project::ElementList&& Project::GetElementList() &&
+{
+  return std::move(elements_);
+}
+
+const Project::ElementList& Project::GetElementList() const&
+{
+  return elements_;
+}
+
+Project::NodeList& Project::GetNodeList() &
+{
+  return nodes_;
+}
+
+Project::NodeList&& Project::GetNodeList() &&
+{
+  return std::move(nodes_);
+}
+
+const Project::NodeList& Project::GetNodeList() const&
+{
+  return nodes_;
+}
+
 
 Element<3>& Project::GetElementAt(size_t index)
 {
--- src/project.h
+++ src/project.h
@@ -3,19 +3,30 @@
 
 #include <memory>
 #include <vector>
+#include <set>
 
 #include "node.h"
 #include "element.h"
 #include "coordinate_system.h"
 
+
 class Project
 {
   using ElementList = std::vector<std::shared_ptr<Element<3>>>;
   using NodeList    = std::vector<std::shared_ptr<Node<3>>>;
+
 public:
 
   void AddElement(Node<3> i, Node<3> j);
-  ElementList& GetElementList();
+
+  ElementList&       GetElementList() &;
+  const ElementList& GetElementList() const&;
+  ElementList&&      GetElementList() &&;
+
+  NodeList&          GetNodeList() &;
+  const NodeList&    GetNodeList() const&;
+  NodeList&&         GetNodeList() &&;
+
   Element<3>& GetElementAt(size_t index);
 
 private:
