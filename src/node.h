d1c56303cbc64e8c37c766aa9f9b17f3ecb3f1cf
ptucki <patryk.tucki@protonmail.com>
2023-12-25 20:40:08 +0100

    Node doxygen

--- src/node.h
+++ src/node.h
@@ -4,17 +4,45 @@
 #include "point.h"
 #include "utilities.h"
 
+/**
+* @brief Class that represents node.
+*
+* @param N node number of cooridnate's dimentions
+*/
 template<int N = 2>
-class Node : public Point<N>
+class Node : public Point<N, double>
 {
 public:
 
+  /**
+  * Constructs a new node
+  * @brief Default constructor
+  * @see template<typename T, typename ...Args> requires arithmetic<T> Node(T arg, Args... args);
+  * @see Node(Point<N> p);
+  */
   Node();
 
+  /**
+  * Constructs a new node
+  * @brief Constructor
+  * @param T - arithmetic type of first argument
+  * @param Args - types of rest arguments
+  * @param arg - first argument
+  * @param args - rest of arguments
+  * @see Node();
+  * @see Node(Point<N> p);
+  */
   template<typename T, typename ...Args>
     requires arithmetic<T>
   Node(T arg, Args... args);
 
+  /**
+  * Constructs a new node
+  * @brief Constructor
+  * @param p - point coordinates
+  * @see Node();
+  * @see template<typename T, typename ...Args> requires arithmetic<T> Node(T arg, Args... args);
+  */
   Node(Point<N> p);
 
 private:
